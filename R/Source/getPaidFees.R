5d255ef0a6858a5bde24f657c5629d48c5e2cc16
artha <aagret@petray.ch>
2018-07-20 10:42:24 +0200

    add soime fees codes

--- R/Source/getPaidFees.R
+++ R/Source/getPaidFees.R
@@ -3,11 +3,12 @@ getPaidFees <- function(db= cashTransactions) {
 #### get Paid Fess ####
 
 	# Caceis list of transaction codes
-	codes   <- c("110-Ext", "110-Ext", "111-VR", "112-VRT", "121-FMT", 
-				 "511-VRT", "F211-OL", "/F222", "F222-TA ", "Interes", 
-				 "commiss", "F201-CO", "F201-NA", "F203-SU", "F204-CU", 
-				 "F204-SA", "F206-DO", "F207-TA", "F207-TR", "INVOICE", 
-				 "OLIS FE", "TA TRAN", "1587", "/F908", "/F211")
+	codes   <- c("110-Ext", "110-Ext", "111-VRT", "112-VRT", "121-FMT", 
+				 "511-VRT", "F211-OL", "/F222/C", "F222-TA", "F222-PO",
+				 "Interes", "commiss", "F201-CO", "F201-NA", "F203-SU", 
+				 "F204-CU", "F204-SA", "F206-DO", "F207-TA", "F207-TR", 
+				 "INVOICE", "OLIS FE", "TA TRAN", "1587QP/", "/F908/T", 
+				 "/F211/C", "F219-SU", "F226-CO", "F901-OT", "/F905/C")
 	
 	tmpData <- db[substr(Description, 1, 7) %in% codes,
 				  sum(Credit) - sum(Debit),
